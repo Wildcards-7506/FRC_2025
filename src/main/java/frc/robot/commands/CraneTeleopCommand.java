87bd820920d9e97be96eeb5e5af534c28317523d
Wildcards7506 <wildcards7506@gmail.com>
2025-01-23 19:03:17 -0600

    Prep for crane logic, button A press-release, robot holds position, A again, robot defaults, A then B, robot goes to B, B again, robot defaults

--- src/main/java/frc/robot/commands/CraneTeleopCommand.java
+++ src/main/java/frc/robot/commands/CraneTeleopCommand.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.pathfinding.LocalADStar.GridPosition;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 import frc.robot.players.PlayerConfigs;
@@ -7,13 +9,13 @@ import frc.robot.players.PlayerConfigs;
 public class CraneTeleopCommand extends Command {
     public CraneTeleopCommand() {
         // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(Robot.crane);
         // Configure additional PID settings here
     }
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {
-    }
+    public void initialize() {}
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
@@ -22,12 +24,22 @@ public class CraneTeleopCommand extends Command {
         //     // Robot.crane.setWristPosition(....);
         // }
 
+        if(PlayerConfigs.gripperOpen)
+            Robot.crane.setGripperPosition(-90);
+        else
+            Robot.crane.setGripperPosition(0);
+
         if(PlayerConfigs.lowPickup) {
             Robot.crane.setWristPosition(0);
-        } else {
-            Robot.crane.setWristPosition(90);
+        } else if(PlayerConfigs.midReef) {
+            Robot.crane.setWristPosition(0);
+        } else if(PlayerConfigs.highReef) {
+            Robot.crane.setWristPosition(0);
+        } else if(PlayerConfigs.shelfReef) {
+            Robot.crane.setWristPosition(0);
+        } else if(PlayerConfigs.stationOrLowReef) {
+            Robot.crane.setWristPosition(0);
         }
-        // Implement the teleop control for the claw here
     }
 
     // Called once the command ends or is interrupted.
