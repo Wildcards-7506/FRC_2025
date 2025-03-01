79f8288e26c86de30effe9884cf8c9be9a4e72f3
Lam Ninh <lnn7447@mavs.uta.edu>
2025-03-01 01:00:02 -0600

    Drivetrain: Fixed reef snaps, snap() has max angular rotation as 90 deg/s or pi/2 rad/s

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -184,10 +184,10 @@ public final class Constants {
   public static final class DriveConstants {
     // Snap constants for reef
     // Angles are relative to the DPAD (CW +), will be inverted in snap method to drive robot (CCW +)
-    public static final double SNAP_UP_LEFT = -30;
-    public static final double SNAP_UP_RIGHT = 30;
-    public static final double SNAP_DOWN_LEFT = -150;
-    public static final double SNAP_DOWN_RIGHT = 150;
+    public static final double SNAP_UP_RIGHT = 60;
+    public static final double SNAP_DOWN_RIGHT = 120;
+    public static final double SNAP_DOWN_LEFT = -120;
+    public static final double SNAP_UP_LEFT = -60;
 
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
